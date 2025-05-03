52ed0ea952f7a2e30ef1c5e51cdf92b3faa948ba
りんご飴196 <kazumac41@gmail.com>
2025-05-04 02:24:42 +0900

    🎉  : 初めてのコミット

---
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
@@ -0,0 +1,13 @@
+package com.github.ringoame196_s_mcPlugin
+
+import com.github.ringoame196_s_mcPlugin.commands.Command
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+    private val plugin = this
+    override fun onEnable() {
+        super.onEnable()
+        val command = getCommand("adview")
+        command!!.setExecutor(Command())
+    }
+}
