95d08ea845ab4e0208a764a43e53663fb70d9d23
りんご飴196 <kazumac41@gmail.com>
2025-05-04 02:43:17 +0900

    ✨ コマンド

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/commands/Command.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/commands/Command.kt
@@ -1,16 +1,38 @@
 package com.github.ringoame196_s_mcPlugin.commands
 
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.command.TabCompleter
+import org.bukkit.entity.Player
 
 class Command : CommandExecutor, TabCompleter {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        if (sender !is Player) {
+            val message = "${ChatColor.RED}このコマンドはプレイヤーのみ実行可能です"
+            sender.sendMessage(message)
+            return true
+        }
+
+        if (args.isEmpty()) return false
+
+        val targetPlayerName = args[0]
+        val targetPlayer = Bukkit.getPlayer(targetPlayerName)
+        if (targetPlayer == null) {
+            val message = "${ChatColor.RED}プレイヤーが見つかりませんでした"
+            sender.sendMessage(message)
+            return true
+        }
+
         return true
     }
 
     override fun onTabComplete(commandSender: CommandSender, command: Command, label: String, args: Array<out String>): MutableList<String>? {
-        return null
+        return when (args.size) {
+            1 -> (Bukkit.getOnlinePlayers().map { it.name }).toMutableList()
+            else -> mutableListOf()
+        }
     }
 }
