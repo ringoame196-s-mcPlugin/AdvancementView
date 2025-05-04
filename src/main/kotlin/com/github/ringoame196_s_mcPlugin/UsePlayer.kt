0fb29e7dea988a42fa9126fca8d8d8ea2e7b5cea
りんご飴196 <kazumac41@gmail.com>
2025-05-04 22:28:52 +0900

    ♻️ リファクタリング

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/UsePlayer.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/UsePlayer.kt
@@ -3,6 +3,6 @@ package com.github.ringoame196_s_mcPlugin
 import org.bukkit.entity.Player
 
 data class UsePlayer(
-	var page: Int,
-	val targetPlayer: Player
+    var page: Int,
+    val targetPlayer: Player
 )
